585f05034f5ba015a4d1e74ac35102235d607f98
joralvmel <joralvmel@gmail.com>
2024-07-24 12:54:58 +0200

    Update createCabin function to handle image upload

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://cfbrsdbxodqrgbviardf.supabase.co";
+export const supabaseUrl = "https://cfbrsdbxodqrgbviardf.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmYnJzZGJ4b2RxcmdidmlhcmRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE3NDExMDQsImV4cCI6MjAzNzMxNzEwNH0.rsiXWMvwnT6J_Js6NDiGDm7JVLUkwcH7dNvtC000Mek";
 const supabase = createClient(supabaseUrl, supabaseKey);

ca6b9e92db835acdc4cf45e8bd6e958605f66c74
joralvmel <joralvmel@gmail.com>
2024-08-05 09:17:39 +0200

    Display Pie Chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -5,7 +5,7 @@ import Spinner from "../../ui/Spinner";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
-// import DurationChart from "./DurationChart";
+import DurationChart from "./DurationChart";
 // import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
@@ -30,8 +30,8 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      {/* <TodayActivity />
-      <DurationChart confirmedStays={confirmedStays} /> */}
+      {/* <TodayActivity /> */}
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );

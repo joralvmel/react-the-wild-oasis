01b954ce3f4f407e9d1c5c5a2f5f6c77f7300959
joralvmel <joralvmel@gmail.com>
2024-08-05 09:06:45 +0200

    Display line chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,7 +4,7 @@ import { useRecentBookings } from "./useRecentBookings";
 import Spinner from "../../ui/Spinner";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
-// import SalesChart from "./SalesChart";
+import SalesChart from "./SalesChart";
 // import DurationChart from "./DurationChart";
 // import TodayActivity from "../check-in-out/TodayActivity";
 
@@ -31,8 +31,8 @@ function DashboardLayout() {
         cabinCount={cabins.length}
       />
       {/* <TodayActivity />
-      <DurationChart confirmedStays={confirmedStays} />
-      <SalesChart bookings={bookings} numDays={numDays} /> */}
+      <DurationChart confirmedStays={confirmedStays} /> */}
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
